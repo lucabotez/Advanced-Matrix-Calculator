524e9e66d2442ad8f71f65fa05da3e7b10bf91ac
bogdan1775 <120658401+bogdan1775@users.noreply.github.com>
2023-05-25 19:57:20 +0300

    Update strassen.h

--- strassen.h
+++ strassen.h
@@ -1,10 +1,10 @@
-// Croiotru Bogdan
+// Croitoru Bogdan
 #ifndef STRASSEN_H_
 #define STRASSEN_H_
 
 #include "list.h"
 
-void strassen(linked_list_t *list, int ind1, int ind2);
+void strassen(linked_list_t *list);
 int **strassen_inmultire(int **mat1, int **mat2, int n);
 int **mat_adunare(int **a, int **b, int n);
 int **mat_scadere(int **a, int **b, int n);
