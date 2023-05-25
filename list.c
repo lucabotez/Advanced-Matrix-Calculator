303f9f2aa02ecd82ba6e3999c6d19ef0af6d60de
bogdan1775 <120658401+bogdan1775@users.noreply.github.com>
2023-05-26 01:16:40 +0300

    Update list.c

--- list.c
+++ list.c
@@ -6,16 +6,6 @@
 
 #include "list.h"
 
-#define DIE(assertion, call_description)  \
-	do {                                  \
-		if (assertion) {                  \
-			fprintf(stderr, "(%s, %d): ", \
-					__FILE__, __LINE__);  \
-			perror(call_description);     \
-			exit(errno);                  \
-		}                                 \
-	} while (0)
-
 #define MAX_STRING_SIZE 64
 
 
