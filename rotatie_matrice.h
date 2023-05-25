5f6b321b48c6c18917b26f4f030fa151c603da26
bogdan1775 <120658401+bogdan1775@users.noreply.github.com>
2023-05-26 01:15:25 +0300

    Update rotatie_matrice.h

--- rotatie_matrice.h
+++ rotatie_matrice.h
@@ -1,10 +1,10 @@
-// Croiotru Bogdan
+// Croitoru Bogdan
 #ifndef ROTATIE_MATRICE_H_
 #define ROTATIE_MATRICE_H_
 
 #include "list.h"
 
-void rotatie_90_grade(linked_list_t*list,int indice);
+void rotatie_90_grade(linked_list_t* list, int indice);
 void rotatie(linked_list_t* list);
 
 #endif /* ROTATIE_MATRICE_H_ */
